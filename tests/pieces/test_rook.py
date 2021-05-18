e4d3bc6ebd372e02bf51d362cb519d86e71510c4
Steven Cheung <stevencheung7@outlook.dk>
2021-05-18 22:38:58 +0200

    test for rook class

---
+++ tests/pieces/test_rook.py
@@ -0,0 +1,43 @@
+from chess_pieces.rook import Rook
+from chess_pieces.schema import Color, Group
+
+
+# Test lower rook moves from position (0, 0)
+def test_pawn_rook_lower_0_0():
+    expected_applied_moves = [
+        (0, 1), (0, 2), (0, 3),
+        (0, 4), (0, 5), (0, 6),
+        (0, 7), (1, 0), (2, 0),
+        (3, 0), (4, 0), (5, 0),
+        (6, 0), (7, 0)
+    ]
+
+    rook = Rook(
+        position=(0, 0),
+        group=Group.lower,
+        color=Color.white
+    )
+    moves = rook.get_applied_moves()
+    for move in moves:
+        assert move in expected_applied_moves
+    assert len(expected_applied_moves) == len(moves)
+
+
+# Test lower rook moves from position (4, 4)
+def test_pawn_rook_lower_4_4():
+    expected_applied_moves = [
+        (4, 0), (4, 1), (4, 2),
+        (4, 3), (4, 5), (4, 6),
+        (4, 7), (0, 4), (1, 4),
+        (2, 4), (3, 4), (5, 4),
+        (6, 4), (7, 4)
+    ]
+    rook = Rook(
+        position=(4, 4),
+        group=Group.lower,
+        color=Color.white
+    )
+    moves = rook.get_applied_moves()
+    for move in moves:
+        assert move in expected_applied_moves
+    assert len(expected_applied_moves) == len(moves)
