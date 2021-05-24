b031cb4387f10312d6d35714edc3cc247cbbb480
Steven Cheung <stevencheung7@outlook.dk>
2021-05-24 22:25:57 +0200

    Typehint to displayer argument

--- board/chess_board.py
+++ board/chess_board.py
@@ -1,9 +1,11 @@
+from typing import Type
+
 from board.engine import Engine
 from board.view import View
 
 
 class ChessBoard(Engine):
-    def __init__(self, config_path: str, displayer: View):
+    def __init__(self, config_path: str, displayer: Type[View]):
         super().__init__(config_path=config_path)
 
         self.displayer = displayer(config_path)
