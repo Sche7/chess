21d8721d1e61285ca0655144f84c6569948f532f
Steven Cheung <stevencheung7@outlook.dk>
2021-12-30 12:32:40 +0100

    update to abstract class View

--- board/view/__init__.py
+++ board/view/__init__.py
@@ -1,19 +1,26 @@
 from nptyping import NDArray
 from board.files import read_yaml
+from abc import abstractmethod
 
 
 class View:
+    """
+    Interface for chess viewer
+    """
     def __init__(self, config_path: str):
         self.config = read_yaml(config_path)
         self.representation = self.config['PIECE_REPRESENTATION']
 
-    def display_board(self, board: NDArray) -> None:
+    @abstractmethod
+    def display_board(self, board: NDArray):
         raise NotImplementedError('method "display_board" is not implemented')
 
+    @abstractmethod
     def await_input(self, board: NDArray):
         raise NotImplementedError('method "await_input" is not implemented')
 
-    def display_player_turn(self, player: str) -> None:
+    @abstractmethod
+    def display_player_turn(self, player: str):
         raise NotImplementedError(
             'method "display_player_turn" is not implemented'
         )
