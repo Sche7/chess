dac7484a0d1c63178c3938d1df9f1601ebcc6cba
Steven Cheung <stevencheung7@outlook.dk>
2022-01-03 21:36:12 +0100

    update play.py

--- play.py
+++ play.py
@@ -3,11 +3,11 @@ Simple py.file to run chess game
 """
 
 from board.game import Chess
-from board.view.terminal_view import TerminalView
+from board.view.displayer_factory import displayer_factory, Displayer
 
 
 config_path = 'config.yml'
-view = TerminalView(config_path=config_path)
+view = displayer_factory(displayer=Displayer.terminal, config_path=config_path)
 chess = Chess(config_path=config_path, displayer=view)
 
 
