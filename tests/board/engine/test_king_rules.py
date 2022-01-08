312bad497dafd3c41c005c0f2ae3c9065c9c393c
Steven Cheung <stevencheung7@outlook.dk>
2022-01-08 15:11:52 +0100

    include more test for king rules

--- tests/board/engine/test_king_rules.py
+++ tests/board/engine/test_king_rules.py
@@ -49,7 +49,7 @@ def test_king_ally_blocking(config_path):
     assert set(king_moves) == set(expected_king_moves)
 
 
-def test_king_suicidal_moves_are_block(config_path):
+def test_king_suicidal_moves_are_blocked(config_path):
     """
     Test that king cannot move to positions where
     death is certain.
@@ -86,3 +86,60 @@ def test_king_suicidal_moves_are_block(config_path):
 
     assert white_king.position == (0, 0)
     assert (1, 1) not in king_moves
+
+
+def test_king_suicidal_moves_check(config_path):
+    """
+    Test that king ally pieces can 'protect'
+    the king by standing between
+    the enemy and the king.
+    """
+    engine = Engine(config_path)
+    engine.initiate_empty_board()
+
+    # See that board is empty
+    assert len(engine.pieces['white']) == 0
+    assert len(engine.pieces['black']) == 0
+
+    # Spawn white king
+    engine.spawn_piece(
+        piece_nr=6,
+        position=(0, 0)
+    )
+
+    # Spawn an black rook
+    engine.spawn_piece(
+        piece_nr=8,
+        position=(1, 7)
+    )
+
+    # Spawn white pawn to 'protect'
+    # the path for the king
+    engine.spawn_piece(
+        piece_nr=1,
+        position=(1, 2)
+    )
+
+    # See that two white pieces has spawned
+    assert len(engine.pieces['white']) == 2
+
+    # See that one black piece has spawned
+    assert len(engine.pieces['black']) == 1
+
+    white_king, white_pawn = engine.pieces['white']
+
+    # See that white king cannot move to (1, 1)
+    king_moves = engine.apply_game_rules(white_king)
+
+    # See that correct pieces are spawned
+    assert white_king.name == 'King'
+    assert white_pawn.name == 'Pawn'
+
+    # See that positions are correct
+    assert white_king.position == (0, 0)
+    assert white_pawn.position == (1, 2)
+
+    # See that king can move because
+    # pawn is 'protecting' the path
+    assert (1, 1) in king_moves
+    assert (1, 0) in king_moves
